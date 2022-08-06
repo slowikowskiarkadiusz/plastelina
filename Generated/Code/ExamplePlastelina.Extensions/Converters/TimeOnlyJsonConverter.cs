63ab14c599a3940427a41e094a8c02bc1b734ac5
Arkadiusz Słowikowski <slowikowskiarkadiusz@gmail.com>
2022-08-06 10:59:56 +0200

    models

---
+++ Generated/Code/ExamplePlastelina.Extensions/Converters/TimeOnlyJsonConverter.cs
@@ -0,0 +1,19 @@
+using System.Globalization;
+using Newtonsoft.Json;
+
+namespace ExamplePlastelina.Converters;
+
+public class TimeOnlyJsonConverter : JsonConverter<TimeOnly>
+{
+    private const string TimeFormat = "HH:mm:ss.FFFFFFF";
+
+    public override TimeOnly ReadJson(JsonReader reader, Type objectType, TimeOnly existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        return TimeOnly.ParseExact((string)reader.Value, TimeFormat, CultureInfo.InvariantCulture);
+    }
+
+    public override void WriteJson(JsonWriter writer, TimeOnly value, JsonSerializer serializer)
+    {
+        writer.WriteValue(value.ToString(TimeFormat, CultureInfo.InvariantCulture));
+    }
+}
